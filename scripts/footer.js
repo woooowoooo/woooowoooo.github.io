1005c0e939801a256cfbfe328c07a2830474db34
Ryan <lolwows111@gmail.com>
2019-08-19 00:18:24 +0800

    File restructure
    Header and footer scripts moved to "scripts" folder, license renamed and moved to "images" folder, references all updated.

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,30 +1,25 @@
 var footer = document.querySelector("footer");
-footer.innerHTML = "";
 function createLicense(type) {
 	var license = document.createElement("a");
 	license.rel = "license";
 	license.href = "http://creativecommons.org/licenses/by-sa/4.0/";
 	if (type == "image") {
-		console.log("License link (image) made");
 		var licenseImg = document.createElement("img");
 		licenseImg.alt = "CC BY-SA 4.0 (image could not be shown)";
-		licenseImg.src = "CC BY-SA license button.png";
 		licenseImg.style.borderWidth = 0;
 		license.appendChild(licenseImg);
 	} else {
-		console.log("License link (regular) made");
 		createText("Creative Commons Attribution-ShareAlike 4.0 International License", license);
 	}
 	footer.appendChild(license);
 }
 function createBr() {
 	footer.appendChild(document.createElement("br"));
-	console.log("Line break made");
 }
 function createText(text, object) {
 	object.appendChild(document.createTextNode(text));
-	console.log("The text that was made is: " + text);
 }
+footer.innerHTML = "";
 createText("Made by Ryan Z.", footer);
 createBr();
 createBr();
