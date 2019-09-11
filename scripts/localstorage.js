ca5acd50ebe86020634dfb3e0399d90391fe96d3
Ryan <lolwows111@gmail.com>
2019-09-11 10:42:24 +0800

    Localstorage move
    Localstorage is moved to scripts folder. The line is also put in with the rest of the scripts.

---
+++ scripts/localstorage.js
@@ -0,0 +1,6 @@
+var theRoot = document.documentElement.style;
+theRoot.setProperty("--primary-color", localStorage.getItem("color1Local"));
+theRoot.setProperty("--secondary-color", localStorage.getItem("color2Local"));
+theRoot.setProperty("--accent-color", localStorage.getItem("color3Local"));
+theRoot.setProperty("--background-color", localStorage.getItem("color4Local"));
+console.log(localStorage.getItem("color1Local"));
--- styles/localstorage.js
+++
@@ -1,9 +0,0 @@
-var theRoot = document.documentElement.style;
-var primaryColor = localStorage.getItem("color1Local");
-var secondaryColor = localStorage.getItem("color2Local");
-var accentColor = localStorage.getItem("color3Local");
-var backgroundColor = localStorage.getItem("color4Local");
-theRoot.setProperty("--primary-color", primaryColor);
-theRoot.setProperty("--secondary-color", secondaryColor);
-theRoot.setProperty("--accent-color", accentColor);
-theRoot.setProperty("--background-color", backgroundColor);
