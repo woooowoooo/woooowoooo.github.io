5282c8352e6d38bcfd13593e1564f6432a7a5259
Ryan <zhangry100@gmail.com>
2021-01-13 13:29:30 -0800

    Favicon
    - Added favicon
    - Added meta description
    - Added createLink function
    - Collapsed the footer functions

--- scripts/combined.js
+++ scripts/combined.js
@@ -6,7 +6,7 @@ function createText(text) {
 const pathArray = location.href.split("/");
 const find = "woooowoooo.github.io";
 const childAmount = pathArray.length - pathArray.indexOf(find) - 2;
-const parentPath = "../";
+const parentPath = "../".repeat(childAmount);
 // Theming
 let root = document.documentElement.style;
 const names = ["background", "primary", "secondary", "accent", "text"];
@@ -14,28 +14,42 @@ function setColor(name) {
 	root.setProperty("--" + name + "-color", localStorage.getItem(name + "Color"));
 }
 names.forEach(setColor);
-// Dark stylesheet
-let head = document.getElementsByTagName("head")[0];
-const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
-let themeType = localStorage.getItem("themeType");
-let darkStylesheet = document.createElement("link");
-darkStylesheet.rel = "stylesheet";
-darkStylesheet.href = parentPath.repeat(childAmount) + "styles/dark.css";
-if (darkQuery.matches && themeType == null || themeType == "dark") {
-	head.appendChild(darkStylesheet);
-}
 // Meta
+let head = document.getElementsByTagName("head")[0];
+let title = document.createElement("title");
+createText.call(title, "woooowoooo's website");
+head.appendChild(title);
 function createMeta(name, content) {
 	let meta = document.createElement("meta");
 	meta.name = name;
 	meta.content = content;
 	head.appendChild(meta);
 }
-createMeta("author", "Ryan Zhang");
+createMeta("description", "A personal website.");
+createMeta("author", "woooowoooo");
 createMeta("viewport", "width=device-width, initial-scale=1.0");
-let title = document.createElement("title");
-createText.call(title, "woooowoooo's website");
-head.appendChild(title);
+function createLink(relation, destination, append) {
+	let link = document.createElement("link");
+	link.rel = relation;
+	link.href = parentPath + destination;
+	if (append) {
+		head.appendChild(link);
+	} else {
+		return link;
+	}
+}
+createLink("icon", "favicon.ico", true);
+// Theme stylesheets
+let darkStylesheet = createLink("stylesheet", "styles/dark.css", false);
+const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+let themeType = localStorage.getItem("themeType");
+if (darkQuery.matches && themeType == null || themeType == "dark") {
+	head.appendChild(darkStylesheet);
+}
+let noneStylesheet = createLink("stylesheet", "styles/none.css", false);
+if (themeType == "none") {
+	head.appendChild(noneStylesheet);
+}
 // Navbar
 let navbar = document.getElementById("navbar");
 navbar.classList.add("hidden"); // If JS is disabled, the navbar should still be shown.
@@ -65,8 +79,8 @@ let createLink = function(element, index) {
 	let li = document.createElement("li");
 	navbar.appendChild(li);
 	let a = document.createElement("a");
-	a.href = parentPath.repeat(childAmount) + element;
 	a.textContent = titleArray[index];
+	a.href = parentPath + page;
 	li.appendChild(a);
 	if (index == activeIndex) {
 		a.classList.add("active");
@@ -100,27 +114,23 @@ closeIcon.addEventListener("click", function () {
 }, false);
 // Footer
 let footer = document.getElementsByTagName("footer")[0];
-function createLicense() {
-	createText.call(footer, "This work is licensed under a ");
-	let license = document.createElement("a");
-	license.rel = "license";
-	license.href = "http://creativecommons.org/licenses/by-sa/4.0/";
-	createText.call(license, "Creative Commons Attribution-ShareAlike 4.0 International License");
-	footer.appendChild(license);
-}
-function createFig() {
-	let fig = document.createElement("figure");
-	let image = document.createElement("img");
-	image.alt = "CC BY-SA 4.0 (image could not be shown)";
-	image.src = parentPath.repeat(childAmount) + "images/CC-BYSA-license.png";
-	image.style.borderWidth = 0;
-	let caption = document.createElement("figcaption");
-	createText.call(caption, "CC BY-SA 4.0");
-	fig.appendChild(image);
-	fig.appendChild(caption);
-	footer.appendChild(fig);
-}
 footer.innerHTML = "";
-createText.call(footer, "Made by Ryan Zhang");
-createFig();
-createLicense();
+createText.call(footer, "Made by woooowoooo");
+// Create license figure
+let fig = document.createElement("figure");
+let image = document.createElement("img");
+image.alt = "CC BY-SA 4.0 (image could not be shown)";
+image.src = parentPath + "images/CC-BYSA-license.png";
+image.style.borderWidth = 0;
+let caption = document.createElement("figcaption");
+createText.call(caption, "CC BY-SA 4.0");
+fig.appendChild(image);
+fig.appendChild(caption);
+footer.appendChild(fig);
+// Create license
+createText.call(footer, "This work is licensed under a ");
+let license = document.createElement("a");
+license.rel = "license";
+license.href = "https://creativecommons.org/licenses/by-sa/4.0/";
+createText.call(license, "Creative Commons Attribution-ShareAlike 4.0 International License");
+footer.appendChild(license);
