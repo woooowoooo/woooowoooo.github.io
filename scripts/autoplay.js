0d1b915ed1e911a134efa00138f38c4480a7f640
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2021-11-26 13:21:29 -0800

    Click to unmute
    Enabled unmuting the 404 page.

--- scripts/autoplay.js
+++ scripts/autoplay.js
@@ -1,7 +1,9 @@
 const video = document.getElementById("rickroll");
 const text = document.getElementsByTagName("h1")[0];
 addEventListener("click", function () {
+	// Play for prize.html, unmute for 404.html
 	video.play();
+	video.muted = false;
 	text.innerHTML = ":)";
 	setTimeout(function () {
 		text.style.display = "none";
